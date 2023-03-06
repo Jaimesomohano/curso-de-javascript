8e22e0ae3a831a635657b8d69a1e1ff4068ed1c6
Jaime Somohano <jaime.somohano@aspentech.com>
2023-03-06 12:27:48 -0600

    prueba

---
+++ hola.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+namespace hola
+{
+    class Program
+    {
+       static void Main(string[] args)
+       {
+          Dictionary<int,int> hola = new Dictionary<int, int>();
+          
+       }
+    }
+}
